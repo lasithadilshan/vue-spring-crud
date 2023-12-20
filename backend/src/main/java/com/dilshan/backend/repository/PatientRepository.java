9f9fbe97892acacf1834323df1937ea450204bfd
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2023-12-20 17:24:21 +0530

    first commit

---
+++ backend/src/main/java/com/dilshan/backend/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package com.dilshan.backend.repository;
+
+import com.dilshan.backend.model.Patient;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends CrudRepository<Patient, Long> {
+
+}
